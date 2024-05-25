c43ee0be4535824ce2743c50c50ea9f749e6d40d
Richa17goyal <richa175goyal@gmail.com>
2024-05-25 09:44:58 +0530

    pyramidPattern.py

---
+++ Assignment1/pyramidPattern.py
@@ -0,0 +1,13 @@
+def pyramid(n):
+    for i in range(n):
+        # Print leading spaces
+        for j in range(n - i - 1):
+            print(' ', end=' ')
+        # Print stars
+        for k in range(2 * i + 1):
+            print('*', end=' ')
+        print()
+
+# Example usage:
+print("\nPyramid Pattern:")
+pyramid(6)
